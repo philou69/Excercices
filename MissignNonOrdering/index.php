9ad8fdfe1126df072ec3da23b5f0f9493db0efd0
BadWolf <phil@pichet.eu>
2017-12-21 16:36:28 +0100

    Refactoring ordering missing

---
+++ MissignNonOrdering/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$numbers = [];
+$indexes = [];
+$number = mt_rand(1, 100000);
+
+// do {
+// 	$randNumber = mt_rand(1, 100000);
+
+// 	if($randNumber != $number && !in_array($randNumber, $numbers)) {
+// 		$numbers[] = $randNumber;
+// 	}
+// 	echo  count($numbers) . "\n";
+// } while (count($numbers) != 99999);
+for ($i=1; $i < 100001; $i++) { 
+	if($i == $number ) {
+		continue;
+	}
+	do {
+		$index = mt_rand(0, 99999);
+	} while (in_array($index, $indexes));
+
+	$indexes[] = $index;
+
+	$numbers[$index] = $i;
+}
+$total = 0;
+$date = microtime(true);
+foreach ($numbers as $num ) {
+	$total += $num;
+}
+$date2 = microtime(true);
+$resutl = array_sum($numbers);
+$date3 = microtime(true);
+
+$findNumber = ((100000 + 1) * 50000) - $total;
+
+echo 'nombre à trouver : ' . $number . "\n";
+echo "vous avez trouvez : " . $findNumber . "\n";
+echo " total " . ($date2 - $date);
+echo " resultat  " . ($date3 - $date2);
--- Missing/index.php
+++ Missing/index.php
@@ -14,43 +14,46 @@ for ($i=1; $i < 1000001; $i++) {
 }
 
 // Now we have to find the remove number
-$index = floor((count($numbers) -1 ) / 2);
+// We create 3 variable
+/*
+start the first range 
+end the last of range 
+find a boolean status of the search
+*/
+$start = 0;
+$end = count($numbers) - 1;
+$find = false;
+
 do {
-	// If the value = the key( index) + 2 and the key = the previous value, we have the position missing 
-	if ($numbers[$index] == $index + 2 && $numbers[$index-1] == $index) {
-		$findNumber = $numbers[$index] - 1;
+	// We calculate the half of the range 
+	// on the first boucle, its the (0+999999) /2 
+	$half =  floor(($start + $end) / 2);
 
-	}
-	if( $numbers[$index] == $index + 1 && $numbers[$index + 1 ] == $index +3) {
-		$findNumber = $numbers[$index] + 1; 
-	}
-	// We have not passe the missing number	
-	if ( $numbers[$index] == ($index + 1) ) {
-		// We get the value of last index or the value ot the length of numbers
-		if( !isset($lastIndex)) {
-			$lastIndex = count($numbers) - 1 ;
-		} elseif ( $lastIndex < $index) {
-			$lastIndex = $index + (($index - $lastIndex) * 2);
+	/*
+	 We check if we are on the next or previous number of the missing number
+	*/
+	if($numbers[$half] + 2 == $numbers[$half+1] ) {
+		$findNumber = $numbers[$half] + 1;
+		$find = true;
+	}elseif($numbers[$half] - 2 == $numbers[$half-1] ) {
+		$findNumber = $numbers[$half] - 1;
+		$find = true;
+	}else {
+		// We haven't find the missing number
+		// If the key +1 = the value, that mean the missing number is up to the position else the missing number is down the position.
+		// than start or end takes the value of hal +/- 1
+		if($numbers[$half] == $half + 1) {
+
+			$start = $half + 1;
+		} else {
+			$end = $half - 1;
 		}
-
-		$nextIndex =  $index + floor(($lastIndex - $index) /2);
-		$lastIndex = $index;
-		$index = $nextIndex;
-		// echo " pas encore  \n";
-	} else {
-		// We get the value of last index or 0
-		if( !isset($lastIndex)) {
-			$lastIndex = 0;
-		} elseif ($lastIndex > $index) {
-			$lastIndex = $index - (($lastIndex - $index) * 2);
-		}
-
-		$nextIndex =  $index - floor(($index - $lastIndex) /2);
-		$lastIndex = $index;
-		$index = $nextIndex;
 	}
 
-} while (!isset($findNumber));
+} while ($find == false);
+
+
+
 
 echo "the missing number is : " . $deletedNumber . "  \n";
-echo " You find : " . $findNumber;
+echo " You find : " . $findNumber . "\n";
