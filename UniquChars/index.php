67f47874c4c2f7835333f31c58f1cb1a9ebe1b93
BadWolf <phil@pichet.eu>
2017-12-30 16:10:51 +0100

    ussing DataStructure

--- UniquChars/index.php
+++ UniquChars/index.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'vendor/autoload.php';
 // debut 12h50 -> fin 14h02 wouhou
 
 
@@ -13,7 +13,7 @@ $notUnique = "Je contiens plusieurs fois les même caractères";
 function isUnique(string $phrase) : bool
 {
 	// We are storing each characters at index of an array
-	$uniqueChars = [];
+	$uniqueChars = new Ds\Set();
 	for ($i=0; $i < strlen($phrase); $i++) {
 		// if the char is a space, we passing because a sentence have mutliple spaces 
 		if($phrase[$i] == " ") {
@@ -21,11 +21,11 @@ function isUnique(string $phrase) : bool
 		}
 
 		// if the char is an index at the array, the sentence have several time the char
-		if(isset($uniqueChars[$phrase[$i]])) {
+		if($uniqueChars->contains($phrase[$i])) {
 			return false;
 		}
 		// else we create the index in the array
-		$uniqueChars[$phrase[$i]] = '0';
+		$uniqueChars->add($phrase[$i]);
 
 	}
 	// After all, the sentence have only unique chars.
