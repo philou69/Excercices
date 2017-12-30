0f10a1e9549a6c046fb769063bdfc3b722127598
BadWolf <phil@pichet.eu>
2017-12-30 13:48:19 +0100

    solution without storage index in variable

--- 2array/index.php
+++ 2array/index.php
@@ -6,34 +6,40 @@ $table1= [1, 5, 8, 20, 35,48,59,75,80, 900];
 $table2 = [2,4,9,45,77,90,120,458,598,785];
 // result = [1,2,4,5,8,9,20,35,48,59,75,77,80,90,120,458,598,785,900]
 $result;
-$index1 = 0;
-$index2 = 0;
 
-// We adding until the length of result = the sum of table1 + table 2
+// the loop is stop when the 2 arrays are empty
 do {
-	// If one index egals the lenght of it array, we adding the rest of the other array and incresse the other index
-	// else we checking which is the small and adding him to result.
-	if($index1 == count($table1)) {
-			$result[] = $table2[$index2]; 
-			$index2++;
-	}elseif($index2 == count($table2)) {
-		$result[] = $table1[$index1]; 
-		$index1++;
-	}elseif($table1[$index1] < $table2[$index2]) {
-		$result[] = $table1[$index1];
-		$index1 ++;
-	}elseif ($table1[$index1] > $table2[$index2]) {
-		$result[] = $table2[$index2];
-		$index2 ++;
-	}else {
-		$result[] = $table1[$index1];
-		$result[] = $table2[$index2];
-		$index1++;
-		$index2++;
+
+	if(empty($table1)) {
+		$result[] = $table2[0];
+		unset($table2[0]);
+		$table2 = array_values($table2);
+	}elseif(empty($table2)) {
+		$result[] = $table1[0];
+		unset($table1[0]);
+		$table1 = array_values($table1);
+	}else{
+		if($table1[0] < $table2[0]) {
+			$result[] = $table1[0];
+			unset($table1[0]);
+			$table1 = array_values($table1);
+		}elseif ($table1[0] > $table2[0]) {
+			$result[] = $table2[0];
+			unset($table2[0]);
+			$table2 = array_values($table2);
+		}else{
+			$result[] = $table1[0];
+			unset($table1[0]);
+			$table1 = array_values($table1);
+			$result[] = $table2[0];
+			unset($table2[0]);
+			$table2 = array_values($table2);
+		}
 	}
 
-} while (count($result) < (count($table1) + count($table2)));
+} while (!empty($table1) || !empty($table2));
+
  // Tadada !
 var_dump($result);
-var_dump($index1);
-var_dump($index2);
+var_dump($table1);
+var_dump($table2);
